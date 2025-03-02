6b7a80a356e9ba9f4e2ea34847dc5e87f7941887
emika <x1186488011@gmail.com>
2025-03-02 21:34:38 +0800

    led+uart ok

--- User/main.c
+++ User/main.c
@@ -17,6 +17,7 @@
 	
 #include "stm32f10x.h"
 #include "bsp_led.h"
+#include "bsp_usart.h"
 
 #define SOFT_DELAY Delay(0x0FFFFF);
 
@@ -29,20 +30,43 @@ void Delay(__IO u32 nCount);
   */
 int main(void)
 {	
+
+  uint8_t LED1 = 0;
+  uint8_t LED2 = 0;
+  uint8_t LED3 = 0;
+
 	/* LED 端口初始化 */
-	LED_GPIO_Config();	 
+	LED_GPIO_Config();
 
-	while (1)
-	{
+  /*初始化USART 配置模式为 115200 8-N-1，中断接收*/
+  USART_Config();
+
+  while (1)
+  {
+
+    LED1 = 1;
+    LED2 = 0;
+    LED3 = 0;
+    Usart_SendByte( DEBUG_USARTx,LED1);
 		LED1_ON;			  // 亮
-		SOFT_DELAY;
-		LED1_OFF;		   // 灭
+    SOFT_DELAY;
+    LED1_OFF;		   // 灭
 
+
+    LED1 = 0;
+    LED2 = 1;
+    LED3 = 0;
+    Usart_SendByte( DEBUG_USARTx,LED2);
 		LED2_ON;			 // 亮
 		SOFT_DELAY;
 		LED2_OFF;		   // 灭
-    
-   LED3_ON;			 // 亮
+
+
+    LED1 = 0;
+    LED2 = 0;
+    LED3 = 1;
+    Usart_SendByte( DEBUG_USARTx,LED3);
+    LED3_ON;			 // 亮
 		SOFT_DELAY;
 		LED3_OFF;		   // 灭
 
